10a224aa9a4e57dc0dc361164a173680a8d7c22e
charly reux <charlyreux@gmail.com>
2024-04-26 16:43:18 +0200

    Added get/{id} method

--- backend/spring-java/src/main/java/com/github/antoinecheron/todomvc/restapi/controllers/TodoController.java
+++ backend/spring-java/src/main/java/com/github/antoinecheron/todomvc/restapi/controllers/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -48,6 +49,12 @@ public class TodoController {
   @DeleteMapping("/{id}") @ResponseStatus(HttpStatus.NO_CONTENT)
   public Mono<Void> deleteTodo(@PathVariable String id) {
     return this.todoService.delete(id);
+  }
+
+  @GetMapping("/{id}") @ResponseStatus(HttpStatus.OK)
+  public Mono<HypermediaRepresentation<Todo>> getTodoById(@PathVariable String id) {
+    return this.todoService.findById(id)
+      .map(TodoController::addHypermediaControls);
   }
 
   public static HypermediaRepresentation<Todo> addHypermediaControls(Todo todo) {
